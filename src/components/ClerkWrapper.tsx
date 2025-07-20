99583bf3f4a4f22854ae19ac0fb5ed9dda649b3e
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-20 16:20:25 +0200

    <feat> add clerk/localizations to translate clerk

--- src/components/ClerkWrapper.tsx
+++ src/components/ClerkWrapper.tsx
@@ -3,6 +3,7 @@ import { useTheme } from "../context/useTheme";
 import App from "../App";
 import { dark } from "@clerk/themes";
 import { useLanguage } from "../hooks/useLanguage";
+import { frFR, esES, enUS } from "@clerk/localizations";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -10,15 +11,23 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
 
+const clerkLocaleMap: Record<string, object> = {
+  en: enUS,
+  es: esES,
+  fr: frFR,
+};
+
 export const ClerkWrapper = () => {
   const { theme } = useTheme();
   const { language } = useLanguage();
+
+  const clerkLocale = clerkLocaleMap[language] || "enUS";
 
   return (
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       appearance={theme === "dark" ? { baseTheme: dark } : {}}
-      localization={{ locale: language }}
+      localization={clerkLocale}
     >
       <App />
     </ClerkProvider>
