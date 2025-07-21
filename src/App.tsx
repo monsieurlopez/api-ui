55c8d17b09ab5d41a7e024bf6d3d1ac6bef5e68b
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-21 22:37:41 +0200

    <feat> create ruoutes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/layout/Header";
 import { PricingCards } from "./components/PricingCards";
 
@@ -5,11 +6,9 @@ const App = () => {
   return (
     <div className="h-[100vh] bg-white text-black dark:bg-gray-900 dark:text-white mx-auto flex flex-col items-center overflow-auto">
       <Header />
-      <div className="container text-center w-full mt-4">
-        <PricingCards />
-        <PricingCards />
-        <PricingCards />
-      </div>
+      <Routes>
+        <Route path="/pricing" element={<PricingCards />} />
+      </Routes>
     </div>
   );
 };
