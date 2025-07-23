d36890c133ba0d71882200d5718c579b5ceef62b
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-23 15:12:57 +0200

    <feat> update button singIn and add button signOut

--- src/components/layout/HeaderBar.tsx
+++ src/components/layout/HeaderBar.tsx
@@ -1,6 +1,11 @@
 import { Layout, Button } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
-import { SignedOut, SignInButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/clerk-react";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 import { useTheme } from "../../context/useTheme";
@@ -31,27 +36,43 @@ export const HeaderBar = ({ collapsed, toggleCollapsed }: Props) => {
   return (
     <Header
       style={{
-        padding: "0 16px",
         backgroundColor: theme === "dark" ? "#001529" : "#fff",
-        display: "flex",
-        alignItems: "center",
         borderLeft: "1px solid #f0f0f0",
       }}
-      className="flex justify-between"
+      className="flex justify-between items-end"
     >
       <Button
         type="text"
         icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         onClick={toggleCollapsed}
-        style={{ fontSize: "20px" }}
+        style={{
+          fontSize: "20px",
+          color: theme === "dark" ? "#fff" : "#001529",
+          margin: 0,
+          padding: 0,
+        }}
       />
       <h1 className="text-4xl font-extrabold text-gray-900 dark:text-gray-100">
         {title}
       </h1>
-      <div className=" w-11">
+      <div className="flex pb-1">
         <SignedOut>
-          <SignInButton />
+          <SignInButton>
+            <Button type={theme === "dark" ? "primary" : "default"}>
+              {t("header.signin")}
+            </Button>
+          </SignInButton>
         </SignedOut>
+        <SignedIn>
+          <SignOutButton>
+            <Button
+              type={theme === "dark" ? "primary" : "default"}
+              size="small"
+            >
+              {t("header.signout")}
+            </Button>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </Header>
   );
