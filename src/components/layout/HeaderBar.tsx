27766a2423659f08b95a3298ecba5d8d01dc4725
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-23 11:49:29 +0200

    <feat> add return layout components and add div right fixe

--- src/components/layout/HeaderBar.tsx
+++ src/components/layout/HeaderBar.tsx
@@ -1,5 +1,7 @@
 import { Layout, Button } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { SignedOut, SignInButton } from "@clerk/clerk-react";
+import { useTranslation } from "react-i18next";
 
 const { Header } = Layout;
 
@@ -9,20 +11,32 @@ type Props = {
   bg: string;
 };
 
-export const HeaderBar = ({ collapsed, toggleCollapsed, bg }: Props) => (
-  <Header
-    style={{
-      padding: "0 16px",
-      background: bg,
-      display: "flex",
-      alignItems: "center",
-    }}
-  >
-    <Button
-      type="text"
-      icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-      onClick={toggleCollapsed}
-      style={{ fontSize: "18px" }}
-    />
-  </Header>
-);
+export const HeaderBar = ({ collapsed, toggleCollapsed, bg }: Props) => {
+  const { t } = useTranslation();
+  return (
+    <Header
+      style={{
+        padding: "0 16px",
+        background: bg,
+        display: "flex",
+        alignItems: "center",
+      }}
+      className="flex justify-between"
+    >
+      <Button
+        type="text"
+        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        onClick={toggleCollapsed}
+        style={{ fontSize: "18px" }}
+      />
+      <h1 className="text-4xl font-extrabold mb-12 text-center lg:mt-10 mt-4">
+        {t("pages.pricing")}
+      </h1>
+      <div className="align-right w-15">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </div>
+    </Header>
+  );
+};
--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -9,23 +9,25 @@ type Props = {
   radius: number;
 };
 
-export const MainContent = ({ bg, radius }: Props) => (
-  <Content style={{ margin: "24px 16px 0" }}>
-    <div
-      style={{
-        padding: 24,
-        minHeight: 360,
-        background: bg,
-        borderRadius: radius,
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/docs" element={<Docs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/settings/account" element={<Account />} />
-      </Routes>
-    </div>
-  </Content>
-);
+export const MainContent = ({ bg, radius }: Props) => {
+  return (
+    <Content style={{ margin: "24px 16px 0" }}>
+      <div
+        style={{
+          padding: 24,
+          minHeight: 360,
+          background: bg,
+          borderRadius: radius,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/settings/account" element={<Account />} />
+        </Routes>
+      </div>
+    </Content>
+  );
+};
