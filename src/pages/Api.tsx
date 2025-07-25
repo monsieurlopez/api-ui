354871a10a65c149bfc296dde1f1876640fe9539
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-25 18:16:34 +0200

    <feat> change text-center to text-left api page

--- src/pages/Api.tsx
+++ src/pages/Api.tsx
@@ -6,7 +6,7 @@ export const Api: React.FC = () => {
 
   return (
     <section className="container text-center w-full flex flex-col mx-auto">
-      <h2 className="text-3xl font-bold mb-12 text-center lg:mt-10 mt-3 text-gray-900 dark:text-gray-100">
+      <h2 className="text-3xl font-bold mb-12 text-left lg:mt-10 mt-3 text-gray-900 dark:text-gray-100">
         {t("pages.api")}
       </h2>
     </section>
