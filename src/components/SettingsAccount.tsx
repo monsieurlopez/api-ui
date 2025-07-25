80e056c11dbaca8fc81e8e55275ba6451eba88a0
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-25 16:52:33 +0200

    <feat> update style settings/account

--- src/components/SettingsAccount.tsx
+++ src/components/SettingsAccount.tsx
@@ -8,6 +8,8 @@ import { useTranslation } from "react-i18next";
 export const SettingsAccount: React.FC = () => {
   const { theme } = useTheme();
   const { t } = useTranslation();
+  const classDiv =
+    "flex flex-col border border-gray-300 dark:border-gray-700 relative justify-between rounded-lg bg-white dark:bg-gray-800 p-6 shadow max-w-[400px] w-full";
 
   return (
     <>
@@ -19,26 +21,25 @@ export const SettingsAccount: React.FC = () => {
               : antdTheme.defaultAlgorithm,
         }}
       >
-        <table className="w-full table-auto">
-          <tbody>
-            <tr className="h-14">
-              <td className="text-left font-medium text-lg text-gray-900 dark:text-gray-100">
-                {t("settings.theme", "Mode")}
-              </td>
-              <td className="text-right">
-                <DarkModeToggle />
-              </td>
-            </tr>
-            <tr className="h-14">
-              <td className="text-left font-medium text-lg text-gray-900 dark:text-gray-100">
-                {t("settings.language", "Language")}
-              </td>
-              <td className="text-right">
-                <LanguageSelector />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <section className="container flex flex-wrap gap-5">
+          <div className={classDiv}>
+            <h3 className="text-left font-medium text-lg text-gray-900 dark:text-gray-100">
+              {t("settings.theme", "Mode")}
+            </h3>
+            <div className="text-left">
+              <DarkModeToggle />
+            </div>
+          </div>
+          <div className={classDiv}>
+            {" "}
+            <h3 className="text-left font-medium text-lg text-gray-900 dark:text-gray-100">
+              {t("settings.language", "Language")}
+            </h3>
+            <div className="text-left">
+              <LanguageSelector />
+            </div>
+          </div>
+        </section>
       </ConfigProvider>
     </>
   );
