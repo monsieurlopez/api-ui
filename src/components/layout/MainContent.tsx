fc917f001eb21bfd501304549d2a91dfa868b4a2
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-29 05:03:17 +0200

    <fix> change path alwais to top page

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,35 +1,48 @@
+import { useEffect } from "react";
 import { Layout } from "antd";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Pricing, Home, Contact, Docs, Account, Api } from "../../pages/pages";
 import { useTheme } from "../../context/useTheme.ts";
 
 const { Content } = Layout;
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 export const MainContent: React.FC = () => {
   const { theme } = useTheme();
   return (
-    <Content
-      style={{
-        backgroundColor: theme === "dark" ? "#001529" : "#fff",
-        borderLeft: "1px solid #f0f0f0",
-      }}
-    >
-      <div
+    <>
+      <ScrollToTop />
+      <Content
         style={{
-          padding: 24,
-          minHeight: 360,
           backgroundColor: theme === "dark" ? "#001529" : "#fff",
+          borderLeft: "1px solid #f0f0f0",
         }}
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/docs" element={<Docs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/settings/account" element={<Account />} />
-          <Route path="/settings/api" element={<Api />} />
-        </Routes>
-      </div>
-    </Content>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 360,
+            backgroundColor: theme === "dark" ? "#001529" : "#fff",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/docs" element={<Docs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/settings/account" element={<Account />} />
+            <Route path="/settings/api" element={<Api />} />
+          </Routes>
+        </div>
+      </Content>
+    </>
   );
 };
