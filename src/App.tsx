4af3e4c0d4eba7ed80316e1eb7a51ed815e20c10
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-29 05:37:52 +0200

    <feat> add FC ScroolToTop

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import { SidebarMenu } from "./components/layout/SideBarMenu";
 import { HeaderBar } from "./components/layout/HeaderBar";
 import { MainContent } from "./components/layout/MainContent";
+import { ScrollToTop } from "./components/global/ScrollToTop";
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
     <Layout className="min-h-screen">
+      <ScrollToTop />
       <SidebarMenu collapsed={collapsed} onCollapse={setCollapsed} />
       <Layout>
         <HeaderBar
---
+++ src/components/global/ScrollToTop.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
+import { ArrowUpOutlined } from "@ant-design/icons";
+import { useTheme } from "../../context/useTheme";
+
+export const ScrollToTop: React.FC = () => {
+  const { theme } = useTheme();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 150) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className="fixed bottom-2 right-2 lg:bottom-6 lg:right-6 z-999">
+      {isVisible && (
+        <Button
+          title="Scroll to top"
+          color={theme === "dark" ? "default" : "primary"}
+          variant={theme === "dark" ? "outlined" : "solid"}
+          className="shadow-lg transition-colors duration-300 ease-in-out"
+          shape="circle"
+          onClick={scrollToTop}
+          icon={<ArrowUpOutlined />}
+        />
+      )}
+    </div>
+  );
+};
