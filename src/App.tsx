0f59ac6a8f7526e46e72cbc9e3dc8db916a7e757
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-30 15:05:23 +0200

    <fix> add ConfigProvider to toogle dark theme in all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Layout } from "antd";
+import { ConfigProvider, theme as antdTheme } from "antd";
+import { useTheme } from "./context/useTheme";
 import { useState } from "react";
 import { SidebarMenu } from "./components/layout/SideBarMenu";
 import { HeaderBar } from "./components/layout/HeaderBar";
@@ -7,19 +9,29 @@ import { ScrollToTop } from "./components/global/ScrollToTop";
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const { theme } = useTheme();
 
   return (
-    <Layout className="min-h-screen">
-      <ScrollToTop />
-      <SidebarMenu collapsed={collapsed} onCollapse={setCollapsed} />
-      <Layout>
-        <HeaderBar
-          collapsed={collapsed}
-          toggleCollapsed={() => setCollapsed(!collapsed)}
-        />
-        <MainContent />
+    <ConfigProvider
+      theme={{
+        algorithm:
+          theme === "dark"
+            ? antdTheme.darkAlgorithm
+            : antdTheme.defaultAlgorithm,
+      }}
+    >
+      <Layout className="min-h-screen">
+        <ScrollToTop />
+        <SidebarMenu collapsed={collapsed} onCollapse={setCollapsed} />
+        <Layout>
+          <HeaderBar
+            collapsed={collapsed}
+            toggleCollapsed={() => setCollapsed(!collapsed)}
+          />
+          <MainContent />
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
--- src/components/setting/account/SettingsAccount.tsx
+++ src/components/setting/account/SettingsAccount.tsx
@@ -1,38 +1,26 @@
 import React from "react";
-import { ConfigProvider, theme as antdTheme, Divider } from "antd";
+import { Divider } from "antd";
 import { DarkModeToggle } from "./DarkModeToogle";
 import { LanguageSelector } from "./LanguageSelector";
-import { useTheme } from "../../../context/useTheme";
 import { BillingPlan } from "./BillingPlan";
 import { BillingCard } from "./BillingCard";
 
 export const SettingsAccount: React.FC = () => {
-  const { theme } = useTheme();
-
   return (
     <>
-      <ConfigProvider
-        theme={{
-          algorithm:
-            theme === "dark"
-              ? antdTheme.darkAlgorithm
-              : antdTheme.defaultAlgorithm,
-        }}
-      >
-        <section className="container flex flex-wrap gap-10 md:gap-6 md:justify-start justify-center mx-auto">
-          <div className="mt-2">
-            <DarkModeToggle />
-          </div>
+      <section className="container flex flex-wrap gap-10 md:gap-6 md:justify-start justify-center mx-auto">
+        <div className="mt-2">
+          <DarkModeToggle />
+        </div>
 
-          <Divider />
-          <div className="mt-2">
-            <LanguageSelector />
-          </div>
-          <Divider />
-          <BillingPlan />
-          <BillingCard />
-        </section>
-      </ConfigProvider>
+        <Divider />
+        <div className="mt-2">
+          <LanguageSelector />
+        </div>
+        <Divider />
+        <BillingPlan />
+        <BillingCard />
+      </section>
     </>
   );
 };
