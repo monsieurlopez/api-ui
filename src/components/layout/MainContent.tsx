f9bf0ab984801a239e0924eff6328db9648026f5
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-07-31 11:48:09 +0200

    <feat> add BrowserRouter and create vercel.json

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Layout } from "antd";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, BrowserRouter } from "react-router-dom";
 import { Pricing, Home, Contact, Docs, Account, Api } from "../../pages/pages";
 import { useTheme } from "../../context/useTheme.ts";
 
@@ -33,14 +33,16 @@ export const MainContent: React.FC = () => {
             backgroundColor: theme === "dark" ? "#001529" : "#fff",
           }}
         >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/docs" element={<Docs />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/settings/account" element={<Account />} />
-            <Route path="/settings/api" element={<Api />} />
-          </Routes>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/docs" element={<Docs />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/settings/account" element={<Account />} />
+              <Route path="/settings/api" element={<Api />} />
+            </Routes>
+          </BrowserRouter>
         </div>
       </Content>
     </>
